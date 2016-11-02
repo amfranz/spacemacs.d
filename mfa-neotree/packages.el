99be43b2815017a33760a42dc95c3253da0e8c72
Michael Franz Aigner <amfranz@gmail.com>
2016-11-02 16:33:09 -0400

    Add all-the-icons, use it as fancy icon theme for neotree

--- mfa-neotree/packages.el
+++ mfa-neotree/packages.el
@@ -1,10 +1,20 @@
-(defconst mfa-neotree-packages '(neotree))
+(defconst mfa-neotree-packages '(all-the-icons neotree))
+
+(defun mfa-neotree/init-all-the-icons ()
+  (use-package all-the-icons
+    :defer t
+    :config
+    ;; Don't waste valuable window real-estate.
+    (setq all-the-icons-scale-factor 1.0)))
 
 (defun mfa-neotree/post-init-neotree ()
   ;; Bind neotree-find to a key.
   (spacemacs/set-leader-keys "fd" #'neotree-find)
 
   (with-eval-after-load 'neotree
-    ;; Enlarge the NeoTree window.
-    (setq neo-window-width 40)
 
+    ;; Make the tree more fancy.
+    (setq neo-theme (if window-system 'icons 'arrow))
+
+    ;; Enlarge the NeoTree window.
+    (setq neo-window-width 40)))

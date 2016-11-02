c071ec4de0ffee483b0a890cf8c138e412abe8ce
Michael Franz Aigner <amfranz@gmail.com>
2016-11-02 16:32:01 -0400

    Remove neotree hook to select current file
    Spacemacs does take care of this.

--- mfa-neotree/packages.el
+++ mfa-neotree/packages.el
@@ -8,11 +8,3 @@
     ;; Enlarge the NeoTree window.
     (setq neo-window-width 40)
 
-    ;; Make neotree-projectile select the current file when opened.
-    (defun neotree-find-project-root-descend-advice (orig-fun &rest args)
-      (let ((cur-file (buffer-file-name)))
-        (prog1
-            (apply orig-fun args)
-          (when (and (neo-global--window-exists-p) cur-file)
-            (neotree-find cur-file)))))
-    (advice-add 'neotree-find-project-root :around #'neotree-find-project-root-descend-advice)))

ddf4a9ad785a77279f7ba90e0cb01ea3fa27211f
Michael Franz Aigner <amfranz@gmail.com>
2017-02-13 21:13:56 -0500

    Add _ as a word character in python mode
    This makes Evils 'replace word' replace entire variables.

--- mfa-python/packages.el
+++ mfa-python/packages.el
@@ -2,5 +2,6 @@
 
 (defun mfa-python/post-init-python ()
   (with-eval-after-load 'python
+    (modify-syntax-entry ?_ "w" python-mode-syntax-table)
     (add-hook 'python-mode-hook (lambda ()
                                   (setq evil-shift-width python-indent-offset)))))

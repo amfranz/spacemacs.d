b808b57fd3d91bb683ee78ccc68a8f4b8f55f59f
Michael Franz Aigner <amfranz@gmail.com>
2017-02-13 21:17:02 -0500

    Remove references to ethan-wspace in editorconfig-mode config

--- mfa-editorconfig/packages.el
+++ mfa-editorconfig/packages.el
@@ -2,15 +2,16 @@
 
 (defun mfa-editorconfig/init-editorconfig ()
   (use-package editorconfig
+    :diminish
+    :init
+    (editorconfig-mode)
     :config
-    (add-to-list 'edconf-custom-hooks
-                 '(lambda (props)
-                    (maphash (lambda (key value)
-                               (progn
-                                 ;; undo require-final-newline to get compatible with ethan-wspace
-                                 ;; should probably be moved to ethan-wspace layer
-                                 (when (eq 'insert_final_newline key)
-                                   (setq require-final-newline nil
-                                         mode-require-final-newline nil))
-                                 (message "editorconfig set %s to %s" key value)))
-                             props)))))
+    (progn
+      (spacemacs|hide-lighter editorconfig-mode)
+      (defvar mfa/edconf-verbose nil)
+      (add-to-list 'edconf-custom-hooks
+                   '(lambda (props)
+                      (maphash (lambda (key value)
+                                 (when mfa/edconf-verbose
+                                   (message "editorconfig set %s to %s" key value)))
+                               props))))))

7cf284616708aa10c416cb8913cc83b177d740a3
Michael Franz Aigner <amfranz@gmail.com>
2017-03-29 00:26:02 -0400

    Make mwim motions play nice with evil repeat
    Also leave it up to mwim to choose where exactly to move to.

--- mfa-mwim/packages.el
+++ mfa-mwim/packages.el
@@ -5,12 +5,14 @@
     :defer t
     :init
     (progn
-      (global-set-key (kbd "C-a") #'mwim-beginning-of-code-or-line)
-      (global-set-key (kbd "C-e") #'mwim-end-of-code-or-line)
-      (global-set-key (kbd "<home>") #'mwim-beginning-of-line-or-code)
-      (global-set-key (kbd "<end>") #'mwim-end-of-line-or-code)
+      (global-set-key (kbd "C-a") #'mwim-beginning)
+      (global-set-key (kbd "C-e") #'mwim-end)
+      (global-set-key (kbd "<home>") #'mwim-beginning)
+      (global-set-key (kbd "<end>") #'mwim-end)
       (with-eval-after-load 'evil
-        (define-key evil-normal-state-map (kbd "C-e") #'mwim-end-of-line-or-code)
-        (define-key evil-motion-state-map (kbd "C-e") #'mwim-end-of-line-or-code)
-        (define-key evil-insert-state-map (kbd "C-e") #'mwim-end-of-line-or-code)
-        (define-key evil-visual-state-map (kbd "C-e") #'mwim-end-of-line-or-code)))))
+        (define-key evil-normal-state-map (kbd "C-e") #'mwim-end)
+        (define-key evil-motion-state-map (kbd "C-e") #'mwim-end)
+        (define-key evil-insert-state-map (kbd "C-e") #'mwim-end)
+        (define-key evil-visual-state-map (kbd "C-e") #'mwim-end)
+        (evil-declare-motion 'mwim-beginning)
+        (evil-declare-motion 'mwim-end)))))

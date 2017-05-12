4a6b10ac53ba76994a3139b07f1a905fe6f561e0
Michael Franz Aigner <amfranz@gmail.com>
2017-05-12 14:36:13 -0400

    Remove vinegar key binding for directory up to avoid dired buffer being killed

--- layers/mfa-dired/packages.el
+++ layers/mfa-dired/packages.el
@@ -1,7 +1,13 @@
-(defconst mfa-dired-packages '(dired-narrow))
+(defconst mfa-dired-packages '(dired-narrow dired))
 
 (defun mfa-dired/init-dired-narrow ()
   (use-package dired-narrow
     :defer t
     :init
     (spacemacs/set-leader-keys-for-major-mode 'dired-mode "/" #'dired-narrow)))
+
+(defun mfa-dired/post-init-dired ()
+  (spacemacs|use-package-add-hook dired
+    :post-config
+    (evil-define-key 'evilified dired-mode-map
+      "-" #'dired-up-directory)))

ef3516bed51427c88cf69fe5c5c806910ad96a5d
Michael Franz Aigner <amfranz@gmail.com>
2017-05-18 13:49:41 -0400

    Whitelist go build commands as safe local variable values

--- layers/mfa-go/packages.el
+++ layers/mfa-go/packages.el
@@ -1,5 +1,9 @@
-(defconst mfa-go-packages '(go-dlv))
+(defconst mfa-go-packages '(go-dlv projectile))
 
 (defun mfa-go/init-go-dlv ()
   (use-package go-dlv
     :defer t))
+
+(defun mfa-go/post-init-projectile ()
+  (push '(projectile-project-test-cmd . "go test") safe-local-variable-values)
+  (push '(projectile-project-compilation-cmd . "go build") safe-local-variable-values))

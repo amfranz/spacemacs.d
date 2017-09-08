af32b48cdcff2f7a94ea2777f86e265d47429f6f
Michael Franz Aigner <amfranz@gmail.com>
2017-09-08 10:32:47 -0400

    Reset treemacs header back to default

--- layers/mfa-treemacs/packages.el
+++ layers/mfa-treemacs/packages.el
@@ -4,12 +4,10 @@
   (use-package treemacs
     :defer t
     :init
-    (progn
-      (setq treemacs-header-function #'treemacs--create-header-projectile)
-      (spacemacs/set-leader-keys
-        "fT" #'treemacs
-        "ft" #'treemacs-toggle
-        "pt" #'treemacs-projectile))
+    (spacemacs/set-leader-keys
+      "fT" #'treemacs
+      "ft" #'treemacs-toggle
+      "pt" #'treemacs-projectile)
     :config
     (require 'treemacs-evil)))
 

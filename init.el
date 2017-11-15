05d0eee58ac17a707d32811b54dde57b2d74476f
Michael Franz Aigner <amfranz@gmail.com>
2017-11-15 09:40:19 -0500

    Be more intelligent about the decision to maximize the initial frame
    This is heavily customized to my environment, but oh well.

--- init.el
+++ init.el
@@ -353,7 +353,7 @@ values."
    ;; If non nil the frame is maximized when Emacs starts up.
    ;; Takes effect only if `dotspacemacs-fullscreen-at-startup' is nil.
    ;; (default nil) (Emacs 24.4+ only)
-   dotspacemacs-maximized-at-startup nil
+   dotspacemacs-maximized-at-startup (and (display-graphic-p) (eq (x-display-pixel-width) 2880))
    ;; A value from the range (0..100), in increasing opacity, which describes
    ;; the transparency level of a frame when it's active or selected.
    ;; Transparency can be toggled through `toggle-transparency'. (default 90)

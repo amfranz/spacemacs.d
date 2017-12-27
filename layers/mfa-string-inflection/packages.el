3462e3dc958e4148cd3d8a9c2fda440611285632
Michael Franz Aigner <amfranz@gmail.com>
2017-12-26 22:31:44 -0500

    Revise key bindings for string-inflection package
      - camelCase -> c
      - CamelCase -> C
      - lisp -> l

--- layers/mfa-string-inflection/packages.el
+++ layers/mfa-string-inflection/packages.el
@@ -6,12 +6,12 @@
     :init
     (progn
       (spacemacs/declare-prefix "xi" "string-inflection")
-      (spacemacs/set-leader-keys "xic" #'string-inflection-camelcase)
-      (spacemacs/set-leader-keys "xie" #'string-inflection-lisp)
+      (spacemacs/set-leader-keys "xiC" #'string-inflection-camelcase)
+      (spacemacs/set-leader-keys "xic" #'string-inflection-lower-camelcase)
       (spacemacs/set-leader-keys "xii" #'string-inflection-all-cycle)
       (spacemacs/set-leader-keys "xij" #'string-inflection-java-style-cycle)
       (spacemacs/set-leader-keys "xik" #'string-inflection-kebab-case)
-      (spacemacs/set-leader-keys "xil" #'string-inflection-lower-camelcase)
+      (spacemacs/set-leader-keys "xil" #'string-inflection-lisp)
       (spacemacs/set-leader-keys "xir" #'string-inflection-ruby-style-cycle)
       (spacemacs/set-leader-keys "xis" #'string-inflection-underscore)
       (spacemacs/set-leader-keys "xit" #'string-inflection-toggle)

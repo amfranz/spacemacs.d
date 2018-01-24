b54579b714466ca4b5d31d99f6bcbede727fa38c
Michael Franz Aigner <amfranz@gmail.com>
2018-01-24 15:27:08 -0500

    Remove obsolete function

--- layers/mfa-dired/funcs.el
+++ layers/mfa-dired/funcs.el
@@ -2,7 +2,3 @@
   "Toggle dired-dwim-target mode."
   :lighter "ⓣ"
   (set (make-local-variable 'dired-dwim-target) dired-dwim-target-mode))
-
-(defun mfa-dired/toggle-dwim-target ()
-  (interactive)
-  (setq dired-dwim-target (not dired-dwim-target)))

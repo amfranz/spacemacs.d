1909f898c8da085ce025f012d73fc558fc5f98dd
Michael Franz Aigner <amfranz@gmail.com>
2018-03-20 09:12:57 -0400

    Remove shell cruft I do not use
    Also adds a key binding to paste in term mode via `Super+v`, which is the same
    key binding I use in my terminal emulator.

--- layers/mfa-shell/packages.el
+++ layers/mfa-shell/packages.el
@@ -1,22 +1,10 @@
-(defconst mfa-shell-packages '(bash-completion eterm-256color shell term))
-
-(defun mfa-shell/init-bash-completion ()
-  (use-package bash-completion
-    :if (configuration-layer/package-usedp 'shell)
-    :defer t))
+(defconst mfa-shell-packages '(eterm-256color term))
 
 (defun mfa-shell/init-eterm-256color ()
   (use-package eterm-256color
-    :if (configuration-layer/package-usedp 'shell)
-    :defer t
-    :init (add-hook 'term-mode-hook #'eterm-256color-mode)))
-
-(defun mfa-shell/post-init-shell ()
-  (when (configuration-layer/package-usedp 'bash-completion)
-    (with-eval-after-load 'shell
-      (add-hook 'shell-dynamic-complete-functions
-                #'bash-completion-dynamic-complete))))
+    :hook (term-mode . eterm-256color-mode)))
 
 (defun mfa-shell/post-init-term ()
   (with-eval-after-load 'term
-    (evil-set-initial-state 'term-mode 'emacs)))
+    (evil-set-initial-state 'term-mode 'emacs)
+    (define-key term-raw-map (kbd "s-v") #'term-paste)))

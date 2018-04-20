168aae4593ff371ad63ca576035c034bd8821ae6
Michael Franz Aigner <amfranz@gmail.com>
2018-04-19 21:17:24 -0400

    Turn off frame maximize at startup
    Since I am starting in daemon mode by default now, the maximize of the initial
    frame does not take effect anyway.

--- init.el
+++ init.el
@@ -404,7 +404,7 @@ values."
    ;; If non nil the frame is maximized when Emacs starts up.
    ;; Takes effect only if `dotspacemacs-fullscreen-at-startup' is nil.
    ;; (default nil) (Emacs 24.4+ only)
-   dotspacemacs-maximized-at-startup (and (display-graphic-p) (memq (x-display-pixel-width) '(1440 2880)))
+   dotspacemacs-maximized-at-startup nil
    ;; A value from the range (0..100), in increasing opacity, which describes
    ;; the transparency level of a frame when it's active or selected.
    ;; Transparency can be toggled through `toggle-transparency'. (default 90)

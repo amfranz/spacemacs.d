ac9fe63022f8dcbcf1090482c944554021c5e770
Michael Franz Aigner <amfranz@gmail.com>
2018-05-02 13:04:15 -0400

    Improve password-store integration

--- layers/mfa-pass/packages.el
+++ layers/mfa-pass/packages.el
@@ -1,7 +1,38 @@
-(defconst mfa-pass-packages '(helm-pass))
+;; -*- lexical-binding: t -*-
+
+(defconst mfa-pass-packages '(auth-source-pass helm-pass pass))
+
+(defun mfa-pass/init-auth-source-pass ()
+  (use-package auth-source-pass
+    :after (auth-source)
+    :config
+    (auth-source-pass-enable)))
 
 (defun mfa-pass/init-helm-pass ()
   (use-package helm-pass
     :defer t
     :init
     (spacemacs/set-leader-keys "ou" #'helm-pass)))
+
+(defun mfa-pass/init-pass ()
+  (use-package pass
+    :defer t
+    :init
+    (spacemacs/set-leader-keys "oU" #'pass)
+    :config
+    (progn
+      ;; Evil-friendly key bindings.
+      (evilified-state-evilify pass-mode pass-mode-map
+        "[" #'pass-prev-directory
+        "]" #'pass-next-directory
+        "j" #'pass-next-entry
+        "k" #'pass-prev-entry
+        "d" #'pass-kill
+        "y" #'pass-copy
+        "gr" #'pass-update-buffer)
+
+      ;; For proper user interface flow, when killing a pass-view buffer the
+      ;; focus should go back to the password-store directory. In order for this
+      ;; to happen, the password-store directory needs to be considered a
+      ;; "useful" buffer.
+      (push (regexp-quote pass-buffer-name) spacemacs-useful-buffers-regexp))))

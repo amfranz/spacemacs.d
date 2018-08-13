095b80b6c1500dc7bf254fe59e68dd1f7f2c0aeb
Michael Franz Aigner <amfranz@gmail.com>
2018-08-12 22:36:07 -0400

    Upgrade to latest treemacs

--- layers/mfa-treemacs/funcs.el
+++ layers/mfa-treemacs/funcs.el
@@ -3,8 +3,3 @@
   (interactive)
   (treemacs-find-file)
   (treemacs-select-window))
-
-(defun mfa-treemacs/goto-parent-node-maybe ()
-  "Select parent of selected node, if possible."
-  (interactive)
-  (-some-> (treemacs-current-button) (button-get :parent) (goto-char)))
--- layers/mfa-treemacs/packages.el
+++ layers/mfa-treemacs/packages.el
@@ -1,16 +1,4 @@
-(defconst mfa-treemacs-packages
-  '((treemacs
-     :location (recipe
-                :fetcher github
-                :repo "amfranz/treemacs"
-                :commit "645b3ac375d6f859599cd7fbbd10d0803ff24d90"
-                :files (:defaults
-                        "icons"
-                        "src/elisp/treemacs*.el"
-                        "src/scripts/treemacs*.py"
-                        (:exclude "src/elisp/treemacs-evil.el"
-                                  "src/elisp/treemacs-projectile.el"))))
-    treemacs-evil treemacs-projectile))
+(defconst mfa-treemacs-packages '(treemacs treemacs-evil treemacs-projectile))
 
 (defun mfa-treemacs/init-treemacs ()
   (use-package treemacs
@@ -24,22 +12,14 @@
       (setq treemacs-silent-refresh t)
       (treemacs-follow-mode t)
       (treemacs-filewatch-mode t)
+      (treemacs-fringe-indicator-mode t)
       (treemacs-resize-icons (* 16 (display-scaling-factor))))))
 
 (defun mfa-treemacs/init-treemacs-evil ()
   (use-package treemacs-evil
-    :after (treemacs)
-    :config
-    (progn
-      (define-key evil-treemacs-state-map (kbd "gi") #'treemacs-change-root)
-      (define-key evil-treemacs-state-map (kbd "gu") #'treemacs-uproot)
-      (define-key evil-treemacs-state-map (kbd "h") #'mfa-treemacs/goto-parent-node-maybe)
-      (define-key evil-treemacs-state-map (kbd "l") #'treemacs-TAB-action))))
+    :after treemacs))
 
 (defun mfa-treemacs/init-treemacs-projectile ()
   (use-package treemacs-projectile
-    :commands (treemacs-projectile-create-header)
-    :init
-    (progn
-      (setq treemacs-header-function #'treemacs-projectile-create-header)
-      (spacemacs/set-leader-keys "pt" #'treemacs-projectile))))
+    :defer t
+    :init (spacemacs/set-leader-keys "pt" #'treemacs-projectile)))

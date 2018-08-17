2f1831c8fed369176e3fea1c2a1da77995af3a85
Michael Franz Aigner <amfranz@gmail.com>
2018-08-17 13:07:41 -0400

    Automatically set up python virtualenv when `pyvenv-workon` is configured as directory/file local variable

--- layers/mfa-python/packages.el
+++ layers/mfa-python/packages.el
@@ -4,4 +4,6 @@
   (with-eval-after-load 'python
     (modify-syntax-entry ?_ "w" python-mode-syntax-table)
     (add-hook 'python-mode-hook (lambda ()
-                                  (setq evil-shift-width python-indent-offset)))))
+                                  (setq evil-shift-width python-indent-offset)))
+    (autoload 'pyvenv-track-virtualenv "pyvenv")
+    (add-hook 'python-mode-local-vars-hook #'pyvenv-track-virtualenv)))

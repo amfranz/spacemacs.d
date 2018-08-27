1700221d1747dcda6e654b88a269471a213ec252
Michael Franz Aigner <amfranz@gmail.com>
2018-08-27 09:31:24 -0400

    Remove unnecessary commented line of code

--- init.el
+++ init.el
@@ -642,9 +642,7 @@ It should only modify the values of Spacemacs settings."
 This function defines the environment variables for your Emacs session. By
 default it calls `spacemacs/load-spacemacs-env' which loads the environment
 variables declared in `~/.spacemacs.env' or `~/.spacemacs.d/.spacemacs.env'.
-See the header of this file for more information."
-  ;; (spacemacs/load-spacemacs-env)
-  )
+See the header of this file for more information.")
 
 (defun dotspacemacs/user-init ()
   "Initialization for user code:

40c033e81dd48215224e4548d791c8b6bd5b1969
Michael Franz Aigner <amfranz@gmail.com>
2018-09-07 10:04:05 -0400

    Minor code style improvements in the `environment` layer

--- layers/mfa-environment/packages.el
+++ layers/mfa-environment/packages.el
@@ -1,6 +1,8 @@
+;; -*- lexical-binding: t -*-
+
 (defconst mfa-environment-packages '(list-environment))
 
 (defun mfa-environment/init-list-environment ()
   (use-package list-environment
     :defer t
-    :init (spacemacs/set-leader-keys "he" #'list-environment)))
+    :init (spacemacs/safe-set-leader-keys "he" #'list-environment)))

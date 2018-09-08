1cbb8d9c610dc2bc8c1c9cd201ec205589818c03
Michael Franz Aigner <amfranz@gmail.com>
2018-09-08 18:55:59 -0400

    Fix Treemacs icon background color

--- layers/mfa-treemacs/funcs.el
+++
@@ -1,5 +0,0 @@
-(defun mfa-treemacs/find-file-select-window ()
-  "Finds the current file in Treemacs and selects the Treemacs window."
-  (interactive)
-  (treemacs-find-file)
-  (treemacs-select-window))
--- layers/mfa-treemacs/packages.el
+++ layers/mfa-treemacs/packages.el
@@ -1,7 +1,24 @@
+;; -*- lexical-binding: t -*-
+
 (defconst mfa-treemacs-packages '(treemacs))
 
 (defun mfa-treemacs/post-init-treemacs ()
-  (setq treemacs-silent-refresh t
-        treemacs-resize-icons (* 16 (display-scaling-factor)))
   (with-eval-after-load 'treemacs
-    (treemacs-fringe-indicator-mode t)))
+    ;; Adjust icon sizes to the DPI of the display.
+    (spacemacs|do-after-display-system-init
+     (treemacs-resize-icons (* 22 (display-scaling-factor))))
+
+    ;; Fixes icon background color. It's broken because Treemacs only adjusts it
+    ;; as an after hook of `enable-theme', but due to lazy loading the theme
+    ;; will have enabled long before treemacs gets loaded. See also:
+    ;; https://github.com/Alexander-Miller/treemacs/issues/148
+    (treemacs--setup-icon-background-colors)
+
+    ;; Enhance the visibility of the currently selected line.
+    (treemacs-fringe-indicator-mode t)
+
+    ;; Moving the root with h/l is non-sensical to me. This rebinds them to
+    ;; functionality that, to me, seems more intuitive to be bound to those
+    ;; keys.
+    (define-key evil-treemacs-state-map "l" #'treemacs-RET-action)
+    (define-key evil-treemacs-state-map "h" #'treemacs-goto-parent-node)))

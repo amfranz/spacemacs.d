40b353e14a539f1e878484e65faa6ce745926d29
Michael Franz Aigner <amfranz@gmail.com>
2018-09-12 10:16:29 -0400

    Remove obsolete commentary

--- init.el
+++ init.el
@@ -224,12 +224,7 @@ This function should only modify configuration layer settings."
      mfa-vdiff
      mfa-visual-ascii-mode
      mfa-xml
-     mfa-yasnippet
-
-     ;; TODO
-     ;; bpr
-     ;; fzf
-     )
+     mfa-yasnippet)
 
    ;; List of additional packages that will be installed without being
    ;; wrapped in a layer. If you need some configuration for these

d1c55c978a1ec465b2ae1aae4d2caba5c4c8127e
Michael Franz Aigner <amfranz@gmail.com>
2018-09-18 09:38:48 -0400

    Housecleaning of the `shell-scripts` layer

---
+++ layers/mfa-shell-scripts/funcs.el
@@ -0,0 +1,5 @@
+;; -*- lexical-binding: t -*-
+
+(defun mfa-shell-scripts//indent-tabs-mode ()
+  (setq indent-tabs-mode t
+        tab-width sh-indentation))
--- layers/mfa-shell-scripts/packages.el
+++ layers/mfa-shell-scripts/packages.el
@@ -1,9 +1,6 @@
+;; -*- lexical-binding: t -*-
+
 (defconst mfa-shell-scripts-packages '(sh-script))
 
 (defun mfa-shell-scripts/post-init-sh-script ()
-  (with-eval-after-load 'sh-script
-    (add-hook 'sh-mode-hook
-              (lambda ()
-                (setq tab-width 4
-                      indent-tabs-mode t
-                      evil-shift-width sh-indentation)))))
+  (add-hook 'sh-mode-hook #'mfa-shell-scripts//indent-tabs-mode t))

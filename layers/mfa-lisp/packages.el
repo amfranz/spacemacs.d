d7a8ab639c40e71dfbf7f62ac1c303540664f4e4
Michael Franz Aigner <amfranz@gmail.com>
2018-09-22 14:24:19 -0400

    Enable `aggressive-indent-mode` in Emacs lisp buffers

--- layers/mfa-lisp/packages.el
+++ layers/mfa-lisp/packages.el
@@ -1,6 +1,11 @@
 ;; -*- lexical-binding: t -*-
 
-(defconst mfa-lisp-packages '(expand-region))
+(defconst mfa-lisp-packages '((emacs-lisp :location built-in)
+                              expand-region))
+
+(defun mfa-lisp/post-init-emacs-lisp ()
+  (when (configuration-layer/package-used-p 'aggressive-indent)
+    (add-hook 'emacs-lisp-mode-hook #'aggressive-indent-mode)))
 
 (defun mfa-lisp/post-init-expand-region ()
   (with-eval-after-load 'expand-region

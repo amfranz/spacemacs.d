b952114bb9013fdb50bc37396fe74464210b7a18
Michael Franz Aigner <amfranz@gmail.com>
2018-09-22 14:31:17 -0400

    Housecleaning of the `devdocs` layer

--- layers/mfa-devdocs/packages.el
+++ layers/mfa-devdocs/packages.el
@@ -1,7 +1,9 @@
+;; -*- lexical-binding: t -*-
+
 (defconst mfa-devdocs-packages '(devdocs))
 
 (defun mfa-devdocs/init-devdocs ()
   (use-package devdocs
     :defer t
     :init
-    (spacemacs/set-leader-keys "o?" 'devdocs-search)))
+    (spacemacs/safe-set-leader-keys "o?" #'devdocs-search)))

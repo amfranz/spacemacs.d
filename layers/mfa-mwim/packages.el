c090fa573fa9155b6e3d587065f0542309f51fb8
Michael Franz Aigner <amfranz@gmail.com>
2018-09-24 22:01:12 -0400

    Housecleaning of the `mwim` layer

--- layers/mfa-mwim/packages.el
+++ layers/mfa-mwim/packages.el
@@ -1,16 +1,13 @@
+;; -*- lexical-binding: t -*-
+
 (defconst mfa-mwim-packages '(mwim))
 
 (defun mfa-mwim/post-init-mwim ()
-  ;; Delay these key bindings to override the defaults
-  (add-hook 'emacs-startup-hook
-            (lambda ()
-              (global-set-key (kbd "C-a") #'mwim-beginning)
-              (global-set-key (kbd "C-e") #'mwim-end)
-              (global-set-key (kbd "<home>") #'mwim-beginning)
-              (global-set-key (kbd "<end>") #'mwim-end)
-              (with-eval-after-load 'evil
-                (define-key evil-normal-state-map (kbd "C-e") #'mwim-end)
-                (define-key evil-motion-state-map (kbd "C-e") #'mwim-end)
-                (define-key evil-visual-state-map (kbd "C-e") #'mwim-end)
-                (evil-declare-motion 'mwim-beginning)
-                (evil-declare-motion 'mwim-end)))))
+  (global-set-key (kbd "C-a") #'mwim-beginning)
+  (global-set-key (kbd "C-e") #'mwim-end)
+  (global-set-key (kbd "<home>") #'mwim-beginning)
+  (global-set-key (kbd "<end>") #'mwim-end)
+  (with-eval-after-load 'evil
+    (define-key evil-motion-state-map (kbd "C-e") #'mwim-end)
+    (evil-declare-motion 'mwim-beginning)
+    (evil-declare-motion 'mwim-end)))

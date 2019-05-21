3d084f910f3d386efad0a1eae6d060767fc4d0fc
Michael Franz Aigner <amfranz@gmail.com>
2019-05-21 01:47:00 -0400

    Enable auto-fill mode in markdown buffers

--- layers/mfa-markdown/packages.el
+++ layers/mfa-markdown/packages.el
@@ -1,6 +1,12 @@
-(defconst mfa-markdown-packages '(flycheck))
+;; -*- lexical-binding: t -*-
+
+(defconst mfa-markdown-packages '(flycheck
+                                  markdown-mode))
 
 (defun mfa-markdown/post-init-flycheck ()
   (spacemacs/enable-flycheck 'markdown-mode)
   (with-eval-after-load 'flycheck
     (flycheck-add-next-checker 'markdown-markdownlint-cli 'proselint)))
+
+(defun mfa-markdown/post-init-markdown-mode ()
+  (add-hook 'markdown-mode-hook #'auto-fill-mode))

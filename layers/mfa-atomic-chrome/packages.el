e90427a5bd751dd6c68c27e5e90e57e8c5981f14
Michael Franz Aigner <amfranz@gmail.com>
2019-05-20 23:37:11 -0400

    Use package-used-p to configure mode integrations of atomic-chrome

--- layers/mfa-atomic-chrome/packages.el
+++ layers/mfa-atomic-chrome/packages.el
@@ -9,7 +9,12 @@
     (when (and (daemonp) (display-assume-graphic-p))
       (add-hook 'spacemacs-post-user-config-hook #'atomic-chrome-start-server))
     :config
-    (setq atomic-chrome-buffer-open-style 'frame
-          atomic-chrome-extension-type-list '(atomic-chrome)
-          atomic-chrome-url-major-mode-alist '(("github" . gfm-mode)
-                                               ("wiki" . dokuwiki-mode)))))
+    (progn
+      (setq atomic-chrome-buffer-open-style 'frame
+            atomic-chrome-extension-type-list '(atomic-chrome))
+
+      (when (configuration-layer/package-used-p 'markdown-mode)
+        (add-to-list 'atomic-chrome-url-major-mode-alist '("github" . gfm-mode)))
+
+      (when (configuration-layer/package-used-p 'dokuwiki-mode)
+        (add-to-list 'atomic-chrome-url-major-mode-alist '("wiki" . dokuwiki-mode))))))

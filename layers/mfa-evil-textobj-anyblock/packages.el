2040c712ede763d3c8bc8e12ec641a45b98a4dad
Michael Franz Aigner <amfranz@gmail.com>
2019-05-22 16:54:38 -0400

    Housecleaning of the mfa-evil-textobj-anyblock layer

--- layers/mfa-evil-textobj-anyblock/packages.el
+++ layers/mfa-evil-textobj-anyblock/packages.el
@@ -1,3 +1,5 @@
+;; -*- lexical-binding: t -*-
+
 (defconst mfa-evil-textobj-anyblock-packages '(evil-textobj-anyblock))
 
 (defun mfa-evil-textobj-anyblock/init-evil-textobj-anyblock ()
@@ -5,5 +7,7 @@
     :defer t
     :init
     (progn
-      (define-key evil-inner-text-objects-map "b" 'evil-textobj-anyblock-inner-block)
-      (define-key evil-outer-text-objects-map "b" 'evil-textobj-anyblock-a-block))))
+      (define-key evil-inner-text-objects-map "b"
+        #'evil-textobj-anyblock-inner-block)
+      (define-key evil-outer-text-objects-map "b"
+        #'evil-textobj-anyblock-a-block))))

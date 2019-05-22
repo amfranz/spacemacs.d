9c21d6130de45377d1d7558d420e81b770972ed2
Michael Franz Aigner <amfranz@gmail.com>
2019-05-22 16:55:34 -0400

    Housecleaning of the mfa-evil-quickscope layer

--- layers/mfa-evil-quickscope/packages.el
+++ layers/mfa-evil-quickscope/packages.el
@@ -1,6 +1,8 @@
+;; -*- lexical-binding: t -*-
+
 (defconst mfa-evil-quickscope-packages '(evil-quickscope))
 
 (defun mfa-evil-quickscope/init-evil-quickscope ()
   (use-package evil-quickscope
-    :config
-    (global-evil-quickscope-mode 1)))
+    :after evil
+    :config (global-evil-quickscope-mode)))

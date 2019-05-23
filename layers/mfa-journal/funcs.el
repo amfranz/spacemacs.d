e76548613450ab0f2d3726f4a8e2e35c3922e19f
Michael Franz Aigner <amfranz@gmail.com>
2019-05-23 09:23:59 -0400

    Move org-journal dir to Syncthing and add a key binding for "today"

--- layers/mfa-journal/funcs.el
+++ layers/mfa-journal/funcs.el
@@ -3,3 +3,7 @@
 (defun mfa-journal//save-carryover ()
   (unless (file-exists-p (buffer-file-name))
     (save-buffer)))
+
+(defun mfa-journal/org-journal-today ()
+  (interactive)
+  (org-journal-new-entry t))
--- layers/mfa-journal/packages.el
+++ layers/mfa-journal/packages.el
@@ -3,14 +3,17 @@
 (defconst mfa-journal-packages '(org-journal))
 
 (defun mfa-journal/post-init-org-journal ()
-  ;; Share my journal to all my devices via Dropbox.
-  (setq org-journal-dir "~/Dropbox/Workspace/org/journal/")
+  ;; Share my journal to all my devices via Syncthing.
+  (setq org-journal-dir "~/Sync/org/journal/")
 
   ;; This is based on suggestions from the Spacemacs manual.
   (setq org-journal-date-prefix "# -*- mode: org-journal -*-\n#+TITLE: "
         org-journal-file-format "%Y-%m-%d.org"
         org-journal-time-format ""
         org-journal-time-prefix "* ")
+
+  ;; extra keybindings for org functionality.
+  (spacemacs/safe-set-leader-keys "ooj" #'mfa-journal/org-journal-today)
 
   ;; When carrying over todo items from a previous journal file, save the new
   ;; journal file immediately so the todo items are sure to not get lost.

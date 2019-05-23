6443992dc36a3c018575629d2577fe2e63e9016f
Michael Franz Aigner <amfranz@gmail.com>
2019-05-23 09:24:34 -0400

    Address cause for company completion to break

--- layers/mfa-yasnippet/packages.el
+++ layers/mfa-yasnippet/packages.el
@@ -29,7 +29,9 @@
   (defun mfa-yasnippet//exit-insert-state (&rest ignored)
     (unless undo-in-progress ;; required?
       (when (evil-insert-state-p)
-        (evil-normal-state))))
+        ;; FIXME breaks company completion
+        ;; (evil-normal-state)
+        )))
   (advice-add 'yas-exit-snippet :after
               #'mfa-yasnippet//exit-insert-state)
 

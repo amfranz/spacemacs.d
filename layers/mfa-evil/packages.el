9c593455f7034df576e3ac69948036c67e174ff6
Michael Franz Aigner <amfranz@gmail.com>
2019-05-23 09:28:32 -0400

    Housecleaning of the evil mode customizations

--- layers/mfa-evil/packages.el
+++ layers/mfa-evil/packages.el
@@ -1,22 +1,16 @@
-(defconst mfa-evil-packages '(evil evil-escape evil-matchit evil-nerd-commenter))
+;; -*- lexical-binding: t -*-
+
+(defconst mfa-evil-packages '(evil
+                              (evil-escape :excluded t)
+                              evil-nerd-commenter))
 
 (defun mfa-evil/post-init-evil ()
-  (with-eval-after-load 'evil
-    ;; Do not move cursor back when exiting insert mode.
-    (setq evil-move-cursor-back nil)))
-
-(defun mfa-evil/post-init-evil-escape ()
-  (with-eval-after-load 'evil-escape
-    ;; A remapped Caps Lock to be ESC/Ctrl via xcape makes evil-escape unnecessary for me.
-    (evil-escape-mode -1)))
-
-(defun mfa-evil/post-init-evil-matchit ()
-  (with-eval-after-load 'evil-matchit
-    ;; Turn on evils advanced tag matching.
-    (global-evil-matchit-mode 1)))
+  ;; Do not move cursor back when exiting insert mode.
+  (setq evil-move-cursor-back nil))
 
 (defun mfa-evil/post-init-evil-nerd-commenter ()
   ;; Register text object for comments.
-  (autoload 'evilnc-get-comment-bounds "evil-nerd-commenter-operator")
+  (autoload 'evilnc-inner-comment "evil-nerd-commenter-operator")
   (define-key evil-inner-text-objects-map "c" #'evilnc-inner-comment)
+  (autoload 'evilnc-outer-commenter "evil-nerd-commenter-operator")
   (define-key evil-outer-text-objects-map "c" #'evilnc-outer-commenter))

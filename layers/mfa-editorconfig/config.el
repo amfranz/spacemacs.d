d705fd7825a104f1758fc4e07e67b5cfff5ebbc2
Michael Franz Aigner <amfranz@gmail.com>
2019-05-23 09:00:58 -0400

    Make editorconfig more reliable
    This ensures that editorconfig customization are run as very last thing in
    hack-local-variables-hook, so they override anything else.

--- layers/mfa-editorconfig/config.el
+++ layers/mfa-editorconfig/config.el
@@ -1,1 +1,5 @@
+;; -*- lexical-binding: t -*-
+
 (defvar mfa-editorconfig-verbose nil)
+
+(defvar before-hack-local-variables-hook nil)
--- layers/mfa-editorconfig/packages.el
+++ layers/mfa-editorconfig/packages.el
@@ -1,5 +1,11 @@
+;; -*- lexical-binding: t -*-
+
 (defconst mfa-editorconfig-packages '(editorconfig))
 
-(defun mfa-editorconfig/post-init-editorconfig ()
-  (with-eval-after-load 'editorconfig
-    (add-hook 'editorconfig-custom-hooks #'mfa-editorconfig//hook)))
+;; Needs to be pre-init, Spacemacs enables the mode in the init section. The
+;; mode hook would not run if we add it after the mode is already enabled.
+(defun mfa-editorconfig/pre-init-editorconfig ()
+  (advice-add 'hack-local-variables
+              :before #'run-before-hack-local-variables-hook)
+  (add-hook 'editorconfig-mode-hook #'mfa-editorconfig//mode-hook)
+  (add-hook 'editorconfig-custom-hooks #'mfa-editorconfig//custom-hook))

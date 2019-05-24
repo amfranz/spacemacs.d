5e6ad101abbb40ef1383be0ab67663a0865e59a2
Michael Franz Aigner <amfranz@gmail.com>
2019-05-24 01:48:47 -0400

    Remove obsolete workaround for helm-files-do-rg
    That fix has appeared upstream now, time for it to go.

--- init.el
+++ init.el
@@ -1280,18 +1280,6 @@ potentially deletes it, after which it can not be autoloaded any more."
       (apply orig-fun args)))
   (advice-add 'helm-do-ag :around #'my-ad-helm-do-ag-rg-sort-files)
 
-  (defun spacemacs/helm-files-do-rg (&optional dir)
-    "Search in files with `rg'."
-    (interactive)
-    ;; --line-number forces line numbers (disabled by default on windows)
-    ;; no --vimgrep because it adds column numbers that wgrep can't handle
-    ;; see https://github.com/syl20bnr/spacemacs/pull/8065
-    (let* ((root-helm-ag-base-command "rg --smart-case --no-heading --color never --line-number")
-           (helm-ag-base-command (if spacemacs-helm-rg-max-column-number
-                                     (concat root-helm-ag-base-command " --max-columns " (number-to-string spacemacs-helm-rg-max-column-number))
-                                   root-helm-ag-base-command)))
-      (helm-do-ag dir)))
-
   ;; Workaround for display issues with squished font glyphs in tooltip windows.
   (setq pos-tip-border-width 0)
   (with-eval-after-load 'tooltip

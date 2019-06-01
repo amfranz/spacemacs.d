2033c8afad6ce92f8f9a1e003f663bd135444566
Michael Franz Aigner <amfranz@gmail.com>
2019-05-31 23:59:07 -0400

    Reorganize customizations of bookmark functionality

---
+++ layers/mfa-bookmarks/funcs.el
@@ -0,0 +1,12 @@
+;; -*- lexical-binding: t -*-
+
+(defun mfa-bookmarks//load-repository ()
+  (require 'bm))
+
+(defun mfa-bookmarks//customize-transient-state ()
+  (defhydra+ spacemacs/bm-transient-state ()
+    "\n [_a_] annotate [_h_] helm [_n_/_p_] next/previous [_s_] show [_t_] toggle [_q_] quit"
+    ("a" #'bm-bookmark-annotate)
+    ("h" #'helm-bm :exit t)
+    ("s" #'bm-show-all :exit t)
+    ("p" #'bm-previous)))
--- layers/mfa-bookmarks/packages.el
+++ layers/mfa-bookmarks/packages.el
@@ -1,64 +1,35 @@
 ;; -*- lexical-binding: t -*-
 
-;; TODO Spacemacs has a `bm' layer now
-(defconst mfa-bookmarks-packages '(bm
-                                   (bookmark+ :location (recipe :fetcher wiki
-                                                                :files ("bookmark+-1.el"
-                                                                        "bookmark+-bmu.el"
-                                                                        "bookmark+-key.el"
-                                                                        "bookmark+-lit.el"
-                                                                        "bookmark+-mac.el"
-                                                                        "bookmark+.el")))
-                                   helm-bm))
+(defconst mfa-bookmarks-packages '(bm helm-bm))
 
 (defun mfa-bookmarks/post-init-bm ()
-  (setq bm-cycle-all-buffers nil
-        bm-highlight-style 'bm-highlight-only-fringe)
+  (setq bm-highlight-style 'bm-highlight-only-fringe)
 
-  ;; To re-define "ab" without triggering a safety check by Spacemacs we need to
-  ;; clear the binding it first.
-  (evil-leader/set-key "ab" nil)
+  ;; Customize the bm transient state.
+  (add-hook 'spacemacs-post-user-config-hook
+            #'mfa-bookmarks//customize-transient-state
+            'append)
 
-  (spacemacs/declare-prefix "ab" "bookmarks")
-  (spacemacs/set-leader-keys
-    "aba" #'bm-bookmark-annotate
-    "abb" #'helm-bm
-    "abi" #'bm-bookmark-show-annotation
-    "abc" #'bm-remove-all-current-buffer
-    "abC" #'bm-remove-all-all-buffers
-    "abn" #'spacemacs/bm-transient-state/bm-next
-    "abp" #'spacemacs/bm-transient-state/bm-previous
-    "abt" #'spacemacs/bm-transient-state/bm-toggle
-    "abs" #'bm-show
-    "abS" #'bm-show-all)
+  ;; The Spacemacs `bm' layer registers this exact same hook in the use-package
+  ;; config section. By registering the hook early, in the use-package init
+  ;; section, we ensure that the `bm' package gets lazily loaded when the user
+  ;; opens the first file.
+  (add-hook 'find-file-hook #'bm-buffer-restore)
 
-  ;; I can never remember when it is `N' or `p', let's just make both work.
-  (spacemacs/transient-state-register-add-bindings 'bm
-    '(("p" #'bm-previous)))
-
-  ;; Eagerly load bookmarks when Emacs starts.
-  ;; TODO maybe this could be moved to a `find-file' advice or some file open
-  ;;      hook. The purpose is to make the bookmarks appear even without having
-  ;;      to trigger the load of `bm' by invoking one of the "ab?" key bindings.
-  ;; (add-hook 'spacemacs-post-user-config-hook
-  ;;           (lambda () (require 'bm)))
-
-  ;; This advice is unnecessary when bookmarks are loaded eagerly.
+  ;; Calling `bm-buffer-restore' to load the bookmark repository is unnecessary
+  ;; extra work for Emacs, a `require' of the `bm' package is sufficient.
   (advice-remove 'spacemacs/bm-transient-state/body #'bm-buffer-restore)
+  (advice-add 'spacemacs/bm-transient-state/body
+              :before #'mfa-bookmarks//load-repository)
 
-  (evil-set-initial-state 'bm-show-mode 'motion)
-  (evil-define-key 'motion bm-show-mode-map
-    "q" #'bm-show-quit-window
-    (kbd "C-g") #'bm-show-quit-window
-    (kbd "ESC") #'bm-show-quit-window
-    "j" #'bm-show-next
-    "k" #'bm-show-prev
-    "v" #'bm-show-bookmark
-    (kbd "RET") #'bm-show-goto-bookmark))
-
-(defun mfa-bookmarks/init-bookmark+ ()
-  (use-package bookmark+
-    :defer t))
+  (with-eval-after-load 'bm
+    (evil-set-initial-state 'bm-show-mode 'motion)
+    (evil-define-key 'motion bm-show-mode-map
+      "q" #'bm-show-quit-window
+      "j" #'bm-show-next
+      "k" #'bm-show-prev
+      "v" #'bm-show-bookmark
+      (kbd "RET") #'bm-show-goto-bookmark)))
 
 (defun mfa-bookmarks/init-helm-bm ()
   (use-package helm-bm

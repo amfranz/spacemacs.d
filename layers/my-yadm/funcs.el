064378eeab916c646c131d2d0a8b7a8a7771a539
Michael Franz Aigner <amfranz@gmail.com>
2019-06-23 01:16:38 -0400

    Disable magit-todos for the yadm repository
    Since the working directory of the yadm repository is the users home directory,
    a scan would search through all the users files, not just those managed by yadm.

--- layers/my-yadm/funcs.el
+++ layers/my-yadm/funcs.el
@@ -4,3 +4,15 @@
   "Show the status of the yadm Git repository in a buffer."
   (interactive)
   (magit-status "/yadm::"))
+
+(defun yadm--magit-todos-manual-scan-ad (orig-fun &rest args)
+  "Turn off automatic scanning by `magit-todos' in `magit-status' buffers that
+visit the yadm repository.
+
+Since the working directory of the yadm repository is the users home directory,
+a scan would search through all the users files, not just those managed by
+yadm."
+  (if (string-prefix-p "/yadm:" default-directory)
+      (let (magit-todos-update)
+        (apply orig-fun args))
+    (apply orig-fun args)))
--- layers/my-yadm/packages.el
+++ layers/my-yadm/packages.el
@@ -1,15 +1,19 @@
 ;; -*- lexical-binding: t -*-
 
-(defconst my-yadm-packages '((tramp :location built-in)))
+(defconst my-yadm-packages '(magit))
 
-(defun my-yadm/init-tramp ()
-  (when (or t (configuration-layer/package-used-p 'magit))
-    (spacemacs/safe-set-leader-keys "oy" #'yadm-status)
-    (with-eval-after-load 'tramp
-      (add-to-list 'tramp-methods
-                   '("yadm"
-                     (tramp-login-program "yadm")
-                     (tramp-login-args (("enter")))
-                     (tramp-login-env (("SHELL") ("/bin/sh")))
-                     (tramp-remote-shell "/bin/sh")
-                     (tramp-remote-shell-args ("-c")))))))
+(defun my-yadm/post-init-magit ()
+  (spacemacs/safe-set-leader-keys "oy" #'yadm-status)
+
+  (when (configuration-layer/package-used-p 'magit-todos)
+    (advice-add 'magit-todos--insert-todos :around
+                #'yadm--magit-todos-manual-scan-ad))
+
+  (with-eval-after-load 'tramp
+    (add-to-list 'tramp-methods
+                 '("yadm"
+                   (tramp-login-program "yadm")
+                   (tramp-login-args (("enter")))
+                   (tramp-login-env (("SHELL") ("/bin/sh")))
+                   (tramp-remote-shell "/bin/sh")
+                   (tramp-remote-shell-args ("-c"))))))

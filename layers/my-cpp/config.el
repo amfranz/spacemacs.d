090ad37cdb238c950f3c401b3e1871766674b1c9
Michael Franz Aigner <amfranz@gmail.com>
2019-07-07 23:39:24 -0400

    Associate correct major mode with extenion-less Qt headers

--- layers/my-cpp/config.el
+++ layers/my-cpp/config.el
@@ -2,3 +2,7 @@
 
 (defvar lsp-ccls-library-directories '("/usr/include/" "/usr/local/include/")
   "List of directories which will considered to be libraries.")
+
+;; Use `c++-mode' for Qt4/5 headers. These mode can not be chosen based on file
+;; extension, because these files have no extension.
+(add-to-list 'auto-mode-alist '("/qt[45]/Q[^/]+/Q[^/]+\\'" . c++-mode))

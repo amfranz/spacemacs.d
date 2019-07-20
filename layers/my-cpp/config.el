b03ee173233598897167304ad1c51339eb0a6f1a
Michael Franz Aigner <amfranz@gmail.com>
2019-07-20 18:24:48 -0400

    Disable auto-indent after paste in C++ mode

--- layers/my-cpp/config.el
+++ layers/my-cpp/config.el
@@ -6,3 +6,6 @@
 ;; Use `c++-mode' for Qt4/5 headers. These mode can not be chosen based on file
 ;; extension, because these files have no extension.
 (add-to-list 'auto-mode-alist '("/qt[45]/Q[^/]+/Q[^/]+\\'" . c++-mode))
+
+;; Auto-indent after paste in C++ makes life very difficult.
+(add-to-list 'spacemacs-indent-sensitive-modes 'c++-mode)

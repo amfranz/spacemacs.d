88c61946237cd2796ba28c3a733fbf76f9dd76af
Michael Franz Aigner <amfranz@gmail.com>
2019-07-20 18:28:26 -0400

    Have company cache LSP completion candidates
    Supposedly that's a performance improvement.

--- layers/my-lsp/packages.el
+++ layers/my-lsp/packages.el
@@ -1,6 +1,9 @@
 ;; -*- lexical-binding: t -*-
 
-(defconst my-lsp-packages '(lsp-mode))
+(defconst my-lsp-packages '(company-lsp lsp-mode))
+
+(defun my-lsp/post-init-company-lsp ()
+  (setq company-lsp-cache-candidates 'auto))
 
 (defun my-lsp/post-init-lsp-mode ()
   (setq lsp-restart 'ignore))

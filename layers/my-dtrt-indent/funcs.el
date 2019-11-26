5d7dcdbf43245f3048440694129ea89fc51048ad
Michael Franz Aigner <amfranz@gmail.com>
2019-11-26 14:52:41 -0500

    Remove advice to adjust evil-shift-width by dtrt-indent
    Turns out the adjustment of evil-shift-width is implemented by dtrt-indent now,
    there is no need any more to augment that functionality.

--- layers/my-dtrt-indent/funcs.el
+++ layers/my-dtrt-indent/funcs.el
@@ -6,11 +6,3 @@
   (unless (or (apply #'derived-mode-p dtrt-indent-mode-blacklist)
               (string= (buffer-name) "*scratch*"))
     (dtrt-indent-mode)))
-
-(defun dtrt-indent//adjust-evil-shift-width ()
-  "Propagate adjustment of indent level to `evil-shift-width'."
-  (let ((indent-offset-variable (nth 2 (dtrt-indent--search-hook-mapping major-mode))))
-    (when (local-variable-p indent-offset-variable)
-      (set (make-local-variable 'evil-shift-width) (symbol-value indent-offset-variable))
-      (when (>= dtrt-indent-verbosity 1)
-        (message "evil-shift-width adjusted to %d" evil-shift-width)))))
--- layers/my-dtrt-indent/packages.el
+++ layers/my-dtrt-indent/packages.el
@@ -7,12 +7,6 @@
     :diminish
     :defer t
     :init
-    (progn
-      ;; Enable `dtrt-indent' mode in all major modes it is whitelisted for.
-      (dolist (mode dtrt-indent-mode-whitelist)
-        (add-hook (intern (concat (symbol-name mode) "-hook"))
-                  #'dtrt-indent//maybe-enable))
-
-      ;; Propagate indent level to `evil-shift-width'.
-      (advice-add 'dtrt-indent-try-set-offset :after
-                  #'dtrt-indent//adjust-evil-shift-width))))
+    (dolist (mode dtrt-indent-mode-whitelist)
+      (add-hook (intern (concat (symbol-name mode) "-hook"))
+                #'dtrt-indent//maybe-enable))))

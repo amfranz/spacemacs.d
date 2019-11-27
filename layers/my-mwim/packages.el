3f9add108769b2a61ddeae078e4b6f3c7d40848b
Michael Franz Aigner <amfranz@gmail.com>
2019-11-26 20:32:57 -0500

    Bind "C-e" to mwim-end in evil-evilified-state as well

--- layers/my-mwim/packages.el
+++ layers/my-mwim/packages.el
@@ -3,25 +3,29 @@
 (defconst my-mwim-packages '(mwim))
 
 (defun my-mwim/post-init-mwim ()
-  (let ((move-beg-fun (lookup-key (current-global-map) (kbd "C-a")))
-        (move-end-fun (lookup-key (current-global-map) (kbd "C-e"))))
-    (global-set-key (kbd "<home>") move-beg-fun)
-    (global-set-key (kbd "<end>") move-end-fun)
-    (with-eval-after-load 'evil
-      (define-key evil-motion-state-map (kbd "C-e") move-end-fun)
-      (dolist (move-fun '(mwim
-                          mwim-beginning
-                          mwim-beginning-of-code
-                          mwim-beginning-of-code-or-line
-                          mwim-beginning-of-code-or-line-or-comment
-                          mwim-beginning-of-comment
-                          mwim-beginning-of-line
-                          mwim-beginning-of-line-function
-                          mwim-beginning-of-line-or-code
-                          mwim-end
-                          mwim-end-of-code
-                          mwim-end-of-code-or-line
-                          mwim-end-of-line
-                          mwim-end-of-line-function
-                          mwim-end-of-line-or-code))
-        (evil-declare-motion move-fun)))))
+  (let ((gmap (current-global-map)))
+    (let ((move-beg-fun (lookup-key gmap (kbd "C-a")))
+          (move-end-fun (lookup-key gmap (kbd "C-e"))))
+      (define-key gmap (kbd "<home>") move-beg-fun)
+      (define-key gmap (kbd "<end>") move-end-fun)
+      (with-eval-after-load 'evil-maps
+        (define-key evil-motion-state-map (kbd "C-e") move-end-fun))
+      (with-eval-after-load 'evil-evilified-state
+        (define-key evil-evilified-state-map (kbd "C-e") move-end-fun))
+      (with-eval-after-load 'evil
+        (dolist (move-fun '(mwim
+                            mwim-beginning
+                            mwim-beginning-of-code
+                            mwim-beginning-of-code-or-line
+                            mwim-beginning-of-code-or-line-or-comment
+                            mwim-beginning-of-comment
+                            mwim-beginning-of-line
+                            mwim-beginning-of-line-function
+                            mwim-beginning-of-line-or-code
+                            mwim-end
+                            mwim-end-of-code
+                            mwim-end-of-code-or-line
+                            mwim-end-of-line
+                            mwim-end-of-line-function
+                            mwim-end-of-line-or-code))
+          (evil-declare-motion move-fun))))))

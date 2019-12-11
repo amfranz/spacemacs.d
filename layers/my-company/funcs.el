dbb5a31c531e83403c20150548ea0618a4c77b40
Michael Franz Aigner <amfranz@gmail.com>
2019-12-11 00:52:41 -0500

    Workaround for occasional errors about dead company-box frames

--- layers/my-company/funcs.el
+++ layers/my-company/funcs.el
@@ -7,3 +7,18 @@ hardcoded bright white text color."
     (custom-theme-set-faces
      'zenburn
      '(company-box-candidate ((t (:inherit default)))))))
+
+;; Sometimes the `company-box' frame dies, which causes an error about a dead
+;; frame whenever the popup would get displayed. This change checks for that
+;; condition and pretends that no such frame was created yet if the current
+;; frame is dead. This not only avoids the errors but also allows the package to
+;; recover from the condition as it will create a new frame.
+(with-eval-after-load 'el-patch
+  (el-patch-feature company-box)
+  (with-eval-after-load 'company-box
+    (el-patch-defun company-box--get-frame ()
+      "Return the child frame."
+      (el-patch-swap
+        (frame-parameter nil 'company-box-frame)
+        (let ((frame (frame-parameter nil 'company-box-frame)))
+          (when (frame-live-p frame) frame))))))

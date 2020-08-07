26f8a7551d4c5fae4ea602f3ba286c84be8eb7f4
Michael Franz Aigner <amfranz@gmail.com>
2020-08-07 09:25:33 -0400

    Make "go to definition in library" work with lsp in python projects

--- layers/my-python/packages.el
+++ layers/my-python/packages.el
@@ -2,11 +2,18 @@
 
 (defconst my-python-packages '(python treemacs))
 
+;; TODO: this is global, can this be made buffer-local?
+;; maybe use `pyvenv-mode' vs `pyvenv-tracking-mode'?
+(defun my-python//activate-virtualenv()
+  (pyvenv-track-virtualenv)
+  (when pyvenv-virtual-env
+    (setq lsp-clients-python-library-directories (list "/usr/" pyvenv-virtual-env))))
+
 (defun my-python/post-init-python ()
   (with-eval-after-load 'python
     (modify-syntax-entry ?_ "w" python-mode-syntax-table)
     (autoload 'pyvenv-track-virtualenv "pyvenv")
-    (add-hook 'python-mode-local-vars-hook #'pyvenv-track-virtualenv)))
+    (add-hook 'python-mode-local-vars-hook #'my-python//activate-virtualenv)))
 
 (defun my-python/post-init-treemacs ()
   (with-eval-after-load 'treemacs

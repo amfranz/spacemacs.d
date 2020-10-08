dd1a26bf5918cdbab056c5285dc359bc1ac1e87b
Michael Franz Aigner <amfranz@gmail.com>
2020-10-08 15:29:27 -0400

    Minor correction to evil-textobj-anyblock initialization
    It didn't actually cause a problem, but there is no guarantee that evil was
    loaded before this block, so it should be wrapped with eval-after-load.

--- layers/my-evil-textobj-anyblock/packages.el
+++ layers/my-evil-textobj-anyblock/packages.el
@@ -6,7 +6,7 @@
   (use-package evil-textobj-anyblock
     :defer t
     :init
-    (progn
+    (with-eval-after-load 'evil-vars
       (define-key evil-inner-text-objects-map "b"
         #'evil-textobj-anyblock-inner-block)
       (define-key evil-outer-text-objects-map "b"

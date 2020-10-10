20e610f4d3c259ece01527a1e7c67a4101da7bf1
Michael Franz Aigner <amfranz@gmail.com>
2020-10-10 15:28:46 -0400

    Only enable dtrt-indent mode in buffers visiting a file
    Initially the idea of excluding the *scratch* buffer was to avoid dtrt-indent.el
    to load eagerly at startup, which it would because my '*scratch*' buffer uses
    text-mode.
    
    The logic was always iffy because it relies on the name of the scratch buffer,
    which de facto always is *scratch*, but there is no real guarantee for that.
    
    After thinking about it, let's exclude all buffers that are not visiting a file.
    That avoid having to match against the name of the scratch buffer, and will also
    exclude some other buffers that dtrt-indent mode is useless for.

--- layers/my-dtrt-indent/funcs.el
+++ layers/my-dtrt-indent/funcs.el
@@ -3,6 +3,6 @@
 (defun dtrt-indent//maybe-enable ()
   "Enable `dtrt-indent' unless the current major mode is blacklisted by
 `dtrt-indent-mode-blacklist'."
-  (unless (or (apply #'derived-mode-p dtrt-indent-mode-blacklist)
-              (string= (buffer-name) "*scratch*"))
+  (when (and buffer-file-name
+             (not (apply #'derived-mode-p dtrt-indent-mode-blacklist)))
     (dtrt-indent-mode)))

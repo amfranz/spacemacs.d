66dbe903437496c568a43155319fe476f60042af
Michael Franz Aigner <amfranz@gmail.com>
2022-05-23 04:59:18 -0400

    Work around erratic buffer popups by rustfmt

--- layers/my-rust/packages.el
+++ layers/my-rust/packages.el
@@ -1,7 +1,8 @@
 ;; -*- lexical-binding: t -*-
 
 (defconst my-rust-packages '(lsp-mode
-                             rust-mode))
+                             rust-mode
+                             rust-rustfmt))
 
 (defun my-rust/post-init-lsp-mode ()
   ;; Save CPU cycles by avoiding compilation after every keypress.
@@ -10,3 +11,11 @@
 
 (defun my-rust/post-init-rust-mode ()
   (add-hook 'rust-mode-hook #'my-rust//sync-fill-column-with-rustcmd))
+
+(defun my-rust/post-init-rust-rustfmt ()
+  ;; When automatic rustfmt on save is turned on and there are syntax errors in
+  ;; the content of the file, rustfmt will open a buffer and move the cursor to
+  ;; the issue. This is a very annoying behavior for a chronic saver like me.
+  ;; Let's not do that.
+  (setq rust-format-show-buffer nil
+        rust-format-goto-problem nil))

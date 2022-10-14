0e892cb2ce1c6b199f2540e8cb5212bcf903d865
Michael Franz Aigner <amfranz@gmail.com>
2022-10-14 10:28:37 -0400

    Synchronizing lsp-mode workspace folders and treemacs projects

--- layers/my-lsp/packages.el
+++ layers/my-lsp/packages.el
@@ -1,9 +1,16 @@
 ;; -*- lexical-binding: t -*-
 
-(defconst my-lsp-packages '(company-lsp lsp-mode))
+(defconst my-lsp-packages '(company-lsp
+                            lsp-mode
+                            lsp-treemacs))
 
 (defun my-lsp/post-init-company-lsp ()
   (setq company-lsp-cache-candidates 'auto))
 
 (defun my-lsp/post-init-lsp-mode ()
   (setq lsp-restart 'ignore))
+
+(defun my-lsp/post-init-lsp-treemacs ()
+  (with-eval-after-load 'treemacs
+    (with-eval-after-load 'lsp-mode
+      (lsp-treemacs-sync-mode))))

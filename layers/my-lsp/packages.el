c4c6b0d80b7edd7f7a80bfaa58a8b0c80d7ea415
Michael Franz Aigner <amfranz@gmail.com>
2022-10-14 10:30:07 -0400

    Add a key binding to show thing-at-point documentation via lsp

--- layers/my-lsp/packages.el
+++ layers/my-lsp/packages.el
@@ -8,7 +8,10 @@
   (setq company-lsp-cache-candidates 'auto))
 
 (defun my-lsp/post-init-lsp-mode ()
-  (setq lsp-restart 'ignore))
+  (setq lsp-restart 'ignore)
+  (with-eval-after-load 'lsp-mode
+    (spacemacs/set-leader-keys-for-minor-mode 'lsp-mode
+      "hd" #'lsp-ui-doc-glance)))
 
 (defun my-lsp/post-init-lsp-treemacs ()
   (with-eval-after-load 'treemacs

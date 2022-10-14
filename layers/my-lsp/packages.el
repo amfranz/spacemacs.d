85d211add08ab40c0765b10d120388fcab045333
Michael Franz Aigner <amfranz@gmail.com>
2022-10-14 10:31:52 -0400

    Do not automatically execute lsp actions if there is only one

--- layers/my-lsp/packages.el
+++ layers/my-lsp/packages.el
@@ -8,7 +8,18 @@
   (setq company-lsp-cache-candidates 'auto))
 
 (defun my-lsp/post-init-lsp-mode ()
+  ;; There is no UI that shows the list of actions beforehand which makes
+  ;; automatic execution often feel like a gamble - sometimes you just don't
+  ;; know what you will get. To avoid this, let's always show the list of
+  ;; actions first, even if there is just one.
+  (setq lsp-auto-execute-action nil)
+
+  ;; If the LSP backend shuts down, just leave it shut down. Don't interactively
+  ;; ask whether to restart it (interrupts flow of thought), don't automatically
+  ;; restart it (if it was due to a crash it will likely just crash again, if it
+  ;; was requested to shut down well then it should stay shut down).
   (setq lsp-restart 'ignore)
+
   (with-eval-after-load 'lsp-mode
     (spacemacs/set-leader-keys-for-minor-mode 'lsp-mode
       "hd" #'lsp-ui-doc-glance)
